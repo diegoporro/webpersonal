cb482902fe66ba8b91753ecef824fc66db983050
Diego Andres Porro Vargas <diegoporro25@gmail.com>
2019-10-22 16:19:46 -0400

    Update settings.py

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -121,10 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
