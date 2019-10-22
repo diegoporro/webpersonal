eb7fd980e4761ed299bc3f9e066d916d707e5c0f
Diego Andres Porro Vargas <diegoporro25@gmail.com>
2019-10-22 16:30:10 -0400

    Update settings.py

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -125,8 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-# Media files
-MEDIA_URL = '/media/'
+# # Media files
+# MEDIA_URL = '/media/'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
